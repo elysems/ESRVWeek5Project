c739930b8e1abfef3d8c261474b1cd738507a654
Elyse Power <elysems@gmail.com>
2014-05-19 16:59:04 -0400

    Revert "changed large photos for better zoom"
    This reverts commit a882e143370dacdf4ea88db8a4a9348ea9e08378.

--- fancybox/jquery.fancybox.js
+++ fancybox/jquery.fancybox.js
@@ -79,7 +79,7 @@
 			arrows     : true,
 			closeBtn   : true,
 			closeClick : false,
-			nextClick  : true,
+			nextClick  : false,
 			mouseWheel : true,
 			autoPlay   : false,
 			playSpeed  : 3000,
